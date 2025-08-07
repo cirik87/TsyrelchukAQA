c8e3e6e44b488390e6b23c33f5dd1fae31ec4cc9
cirik87 <cirik87@mail.ru>
2025-08-07 16:19:26 +0300

    User-agent

---
+++ Lesson_17_Check_box.py
@@ -0,0 +1,34 @@
+# import time
+# from os import times
+#
+# from selenium import webdriver
+#
+#
+#
+# driver = webdriver.Chrome()
+# driver.get("https://demoqa.com/radio-button")
+#
+#
+# # checkbox = driver.find_element("xpath", "//label[@for='tree-node-home']")
+# # checkbox.click()
+# # time.sleep(3)
+# YES_RADIO_LABEL = ("xpath", "//label[@for='yesRadio']")
+# YES_RADIO_BUTTON = ("xpath", "//input[@id='yesRadio']")
+#
+#
+# IMPRESSIVE_RADIO_LABEL = ("xpath", "//label[@for='impressiveRadio']")
+# IMPRESSIVE_RADIO_BUTTON = ("xpath", "//input[@id='impressiveRadio']")
+#
+# NO_RADIO_BUTTON = ("xpath", "//input[@id='noRadio']")
+#
+# print(driver.find_element(*NO_RADIO_BUTTON).is_enabled())
+#
+# driver.find_element(*YES_RADIO_LABEL).click()
+# print(driver.find_element(*YES_RADIO_BUTTON).is_selected())
+# time.sleep(5)
+#
+# driver.find_element(*IMPRESSIVE_RADIO_LABEL).click()
+# print(driver.find_element(*IMPRESSIVE_RADIO_BUTTON).is_selected())
+# time.sleep(5)
+
+
