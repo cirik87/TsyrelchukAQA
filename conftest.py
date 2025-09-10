45f119a0382d6161bbebc190aed3825fe2901726
cirik87 <cirik87@mail.ru>
2025-09-10 09:36:10 +0300

    Финальное ДЗ по Selenium

---
+++ conftest.py
@@ -0,0 +1,25 @@
+# import pytest
+# from collections import namedtuple
+# from faker import Faker
+
+#
+# faker = Faker()
+#
+#
+# @pytest.fixture()
+# def user_data(request):
+#     request.cls.login = faker.user_name()
+#     request.cls.password = faker.password()
+#
+import pytest
+import time
+from selenium import webdriver
+
+
+@pytest.fixture(autouse=True)
+def get_driver(request):
+
+    driver = webdriver.Chrome()
+    request.cls.driver = driver
+    yield # Передается управление тесту
+    driver.quit()
